989c01eea6926880f64f9a5679d89deb39c54c0c
marco-tuzza <marcotuzza@hotmail.it>
2025-05-27 15:07:02 +0200

    Refactor SCSS imports and add homepage jumbotron section
    Replaced `@import` with `@use` in SCSS files for better scoping. Updated styles and variables usage to align with the new import method. Added a full-page jumbotron section with headings and a button to the homepage for improved visual appeal.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,11 @@
     @vite(['resources/css/home.scss', 'resources/js/app.js'])
 @endpush
 @section('content')
-    Test content
+    <div id="jumbotron">
+        <div id="jumbotron-text">
+            <h1>On Hair <br> Barber Shop</h1>
+            <h2>Accendiamo il tuo stile</h2>
+            <a class="button" href="/">PRENOTA ORA UN APPUNTAMENTO</a>
+        </div>
+    </div>
 @stop
