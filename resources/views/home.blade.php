bd77f02c8a3f4de008523cf543c0b8ffea1f898a
marco-tuzza <marcotuzza@hotmail.it>
2025-05-27 20:29:17 +0200

    Refactor SCSS structure and enhance styling for homepage
    Consolidated global styles into a new `general.scss` file for reuse across components. Updated colors for consistency and improved the `home.scss` layout by reorganizing sections. Added a new "Our Services" section to enrich the homepage content.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @push('styles')
-    @vite(['resources/css/home.scss', 'resources/js/app.js'])
+    @vite(['resources/css/home.scss'])
 @endpush
 @section('content')
     <section id="jumbotron">
@@ -15,23 +15,35 @@
             <div class="barber-text">
                 <h3>Andrea Miserendino</h3>
                 <span>Master Barber</span>
-                <p>Creiamo lo stile intorno alla tua identità, valorizzando la natura. Il nostro lavoro è la nostra grande passione. L’acconciatura è al centro di un progetto più grande: accendere il tuo stile. Qualunque esso sia. </p>
+                <p>Creiamo lo stile intorno alla tua identità, valorizzando la natura. Il nostro lavoro è la nostra grande passione. L’acconciatura è al centro di un progetto più grande: accendere il tuo stile. Qualunque esso sia.</p>
+                <a class="button" href="/">Prenota ora un appuntamento</a>
             </div>
-            <div class="barber-image" id="first-barber">
-
+            <div class="barber-image" id="second-barber">
+                <img src="https://www.onhair.men/wp-content/uploads/2023/01/ANDREA_MISERENTINO.jpg" alt="Foto di Andrea Miserendino, barbiere" loading="lazy">
             </div>
         </div>
         <div class="barber-container">
             <div class="barber-image" id="second-barber">
-                <img src="https://www.onhair.men/wp-content/uploads/2023/01/natale_rifici.jpg" alt="Foto di Natale Rifici, barbiere">
+                <img src="https://www.onhair.men/wp-content/uploads/2023/01/natale_rifici.jpg" alt="Foto di Natale Rifici, barbiere" loading="lazy">
             </div>
             <div class="barber-text">
                 <h3>Natale Rifici</h3>
                 <span>Master Barber</span>
-                <p>La qualità è quello che fai quando nessuno guarda.  Ogni dettaglio di On Hair è costruito per rispondere alla nostra personale esigenza di qualità.  Ma il test supremo è la soddisfazione del cliente, il suo sorriso quando si specchia. </p>
-                <p>Per arrivare al tuo sorriso abbiamo fatto tantissima strada e non ci siamo fermati di fronte alle difficoltà. Dopo 18 mesi di ricerca e laboratori siamo riusciti nell’impresa di avere una linea di prodotti totalmente nostra. Dallo shampoo, ai balsami alle creme, abbiamo creato quello che volevamo per poter assistere il cliente al meglio, per dargli il piacere che merita. </p>
+                <span id="barber-text-title">On Hair Thinking</span>
+                <p>La qualità è quello che fai quando nessuno guarda.  Ogni dettaglio di On Hair è costruito per rispondere alla nostra personale esigenza di qualità.  Ma il test supremo è la soddisfazione del cliente, il suo sorriso quando si specchia.</p>
+                <p>Per arrivare al tuo sorriso abbiamo fatto tantissima strada e non ci siamo fermati di fronte alle difficoltà. Dopo 18 mesi di ricerca e laboratori siamo riusciti nell’impresa di avere una linea di prodotti totalmente nostra. Dallo shampoo, ai balsami alle creme, abbiamo creato quello che volevamo per poter assistere il cliente al meglio, per dargli il piacere che merita.</p>
                 <p>Essere On Hair vuol dire avere un contatto diretto, personale con chi si affida a noi. Look, stile ma anche relax, cura totale della persona, momenti di invidiabile benessere. Pensiamo che sia giusto così e ogni giorno lavoriamo per farlo meglio.</p>
+                <a class="button" href="/">Prenota ora un appuntamento</a>
             </div>
+        </div>
+    </section>
+    <section id="services">
+        <h2>I nostri servizi</h2>
+        <div id="services-container">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     </section>
 @stop
