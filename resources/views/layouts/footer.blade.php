5471e5644090455e5391699ea82322e67fc245fa
marco-tuzza <marcotuzza@hotmail.it>
2025-06-06 22:14:22 +0200

    Make phone and email in footer clickable; update link styling in SCSS

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,8 +6,12 @@
             <span id="footer-text-address">Via Cesare Battisti, 189 <br>
             98123 - Messina</span>
             <span id="footer-text-time">Martedì - Sabato 8<sup>00</sup> - 19<sup>00</sup></span>
-            <span id="footer-text-phone"><img src="{{ asset('images/icons/phone.svg') }}" alt="Phone icon" width="40" height="18">327 161 8752</span>
-            <span id="footer-text-email"><img src="{{ asset('images/icons/email.svg') }}" alt="Email icon" width="40" height="18">info@onhair.men</span>
+            <a href="tel:+393271618752" id="footer-text-phone">
+                <img src="{{ asset('images/icons/phone.svg') }}" alt="Phone icon" width="40" height="18">327 161 8752
+            </a>
+            <a href="mailto:info@onhair.men" id="footer-text-email">
+                <img src="{{ asset('images/icons/email.svg') }}" alt="Email icon" width="40" height="18">info@onhair.men
+            </a>
         </div>
         <div id="footer-map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3135.95821784386!2d15.548790531603911!3d38.187644631107325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13144fd603bb9ad1%3A0xdd1de019a5ef593e!2sOn%20Hair%202.0!5e0!3m2!1sit!2sit!4v1674933243946!5m2!1sit!2sit" width="600" height="450" style="border:0" allowfullscreen="" title="Mappa con l'ubicazione di On Hair barber shop"></iframe>
